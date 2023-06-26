5ce8d06f0625414099aacc627d508dac8047a0ab
Timothe Lebrun <timothe463@icloud.com>
2023-06-26 22:56:38 +0200

    get data / display data --index

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,34 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+  let url = "../../data/photographers.json";
+  let options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    let allMembersOrigin = await fetch(url, options);
+    let allMembers = await allMembersOrigin.json();
+    return allMembers;
+  } catch {
+    console.log(err);
+  }
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,32 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, id, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const anchor = document.createElement("a");
+    anchor.href = `../../photographer.html?id=${id}`;
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", `Photo de ${name}`);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const location = document.createElement("h3");
+    location.textContent = `${city}, ${country}`;
+    const catchPhrase = document.createElement("p");
+    catchPhrase.textContent = `${tagline}`;
+    const priceTag = document.createElement("p");
+    priceTag.textContent = `${price}€/jour`;
+    // Append to anchor
+    anchor.appendChild(img);
+    anchor.appendChild(h2);
+    // Append to article
+    article.appendChild(anchor);
+    article.appendChild(location);
+    article.appendChild(catchPhrase);
+    article.appendChild(priceTag);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
