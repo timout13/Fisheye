8ab310b02cdb7893a265a4375c64dad6f8cf0822
Timothe Lebrun <timothe463@icloud.com>
2023-07-06 09:28:47 +0200

    style / aria homepage + start photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,29 @@
 //Mettre le code JavaScript lié à la page photographer.html
+let params = new URLSearchParams(document.location.search);
+
+async function getPhotographer() {
+    let url = "../../data/photographers.json";
+    let options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        },
+      body:{id:params.get("id")}
+    };
+    try {
+      let allMembersOrigin = await fetch(url, options);
+      let allMembers = await allMembersOrigin.json();
+      return allMembers;
+    } catch {
+      console.log(err);
+    }
+}
+  
+async function init() {
+    // Récupère les datas des photographes
+    const { photographer } = await getPhotographer();
+    //displayData(photographers);
+    console.log(photographer);
+  }
+  
+  init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -5,18 +5,28 @@ function photographerTemplate(data) {
 
   function getUserCardDOM() {
     const article = document.createElement("article");
+    article.classList.add("photographer_card");
+    article.setAttribute('aria-labelledby', `photograph-${id}`);
     const anchor = document.createElement("a");
+    anchor.classList.add("photographer_card_header");
+    anchor.setAttribute('aria-label', `Page de ${name}`);
     anchor.href = `../../photographer.html?id=${id}`;
     const img = document.createElement("img");
+    img.classList.add("photographer_card_header_img");
     img.setAttribute("src", picture);
     img.setAttribute("alt", `Photo de ${name}`);
     const h2 = document.createElement("h2");
+    h2.classList.add("photographer_card_header_name");
+    h2.id=`photograph_${id}`
     h2.textContent = name;
     const location = document.createElement("h3");
+    location.classList.add("photographer_card_location");
     location.textContent = `${city}, ${country}`;
     const catchPhrase = document.createElement("p");
+    catchPhrase.classList.add("photographer_card_desc");
     catchPhrase.textContent = `${tagline}`;
     const priceTag = document.createElement("p");
+    priceTag.classList.add("photographer_card_price");
     priceTag.textContent = `${price}€/jour`;
     // Append to anchor
     anchor.appendChild(img);
