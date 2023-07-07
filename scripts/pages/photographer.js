0cc03f746966615bf8e6ebd299e178d33c56c824
Timothe Lebrun <timothe463@icloud.com>
2023-07-07 16:48:05 +0200

    Data on photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,29 +1,59 @@
 //Mettre le code JavaScript lié à la page photographer.html
-let params = new URLSearchParams(document.location.search);
 
 async function getPhotographer() {
-    let url = "../../data/photographers.json";
-    let options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        },
-      body:{id:params.get("id")}
-    };
-    try {
-      let allMembersOrigin = await fetch(url, options);
-      let allMembers = await allMembersOrigin.json();
-      return allMembers;
-    } catch {
-      console.log(err);
-    }
-}
-  
-async function init() {
-    // Récupère les datas des photographes
-    const { photographer } = await getPhotographer();
-    //displayData(photographers);
-    console.log(photographer);
+  let url = "../../data/photographers.json";
+  let options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    let allMembersOrigin = await fetch(url, options);
+    let allMembers = await allMembersOrigin.json();
+    return allMembers;
+  } catch {
+    console.log(err);
   }
-  
-  init();
+}
+const getAuthorImgs = (allImgs, idAuthor) => {
+  const authorImgs = [];
+  allImgs.forEach((img) => {
+    img.photographerId === idAuthor && authorImgs.push(img);
+  });
+  return authorImgs;
+};
+const getAuthorById = (idAuthor, allAuthor) => {
+  for (const author of allAuthor) {
+    if (author.id == idAuthor) {
+      return author;
+    }
+  }
+  return null;
+};
+
+async function displayData(authorImgs, author) {
+  const photographersSection = document.querySelector(".photograph-header");
+  const photographWP = document.querySelector(".photograph-wp");
+  const photographerModel = photographerTemplate(author);
+  const { img, h2, location, catchPhrase, priceTag } =
+  photographerModel.getAuthorBlock();
+  photographersSection.append(img, h2, location, catchPhrase, priceTag);
+  authorImgs.forEach(img => {
+    const imgElement = imageTemplate(img);
+    photographWP.appendChild(imgElement);
+  })
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const getAuthor = new URLSearchParams(document.location.search);
+  const idAuthor = Number(getAuthor.get("id"));
+  const { media, photographers } = await getPhotographer();
+  const authorImgs = getAuthorImgs(media, idAuthor);
+
+  const author = getAuthorById(idAuthor, photographers);
+  displayData(authorImgs, author);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -3,21 +3,14 @@ function photographerTemplate(data) {
 
   const picture = `assets/photographers/${portrait}`;
 
-  function getUserCardDOM() {
-    const article = document.createElement("article");
-    article.classList.add("photographer_card");
-    article.setAttribute('aria-labelledby', `photograph-${id}`);
-    const anchor = document.createElement("a");
-    anchor.classList.add("photographer_card_header");
-    anchor.setAttribute('aria-label', `Page de ${name}`);
-    anchor.href = `../../photographer.html?id=${id}`;
+  const createMainElement = () => {
     const img = document.createElement("img");
     img.classList.add("photographer_card_header_img");
     img.setAttribute("src", picture);
     img.setAttribute("alt", `Photo de ${name}`);
     const h2 = document.createElement("h2");
     h2.classList.add("photographer_card_header_name");
-    h2.id=`photograph_${id}`
+    h2.id = `photograph_${id}`;
     h2.textContent = name;
     const location = document.createElement("h3");
     location.classList.add("photographer_card_location");
@@ -28,6 +21,19 @@ function photographerTemplate(data) {
     const priceTag = document.createElement("p");
     priceTag.classList.add("photographer_card_price");
     priceTag.textContent = `${price}€/jour`;
+    return { img, h2, location, catchPhrase, priceTag };
+  };
+/* AJOUTER ARIA LABEL À DESC PRICE LOCATION HOMEPAGE */
+  // For Homepage
+  function getUserCardDOM() {
+    const { img, h2, location, catchPhrase, priceTag } = createMainElement();
+    const article = document.createElement("article");
+    const anchor = document.createElement("a");
+    article.classList.add("photographer_card");
+    article.setAttribute("aria-labelledby", `photograph-${id}`);
+    anchor.classList.add("photographer_card_header");
+    anchor.setAttribute("aria-label", `Page de ${name}`);
+    anchor.href = `../../photographer.html?id=${id}`;
     // Append to anchor
     anchor.appendChild(img);
     anchor.appendChild(h2);
@@ -38,5 +44,15 @@ function photographerTemplate(data) {
     article.appendChild(priceTag);
     return article;
   }
-  return { name, picture, getUserCardDOM };
+
+  //For Photographer page
+  const getAuthorBlock = () => {
+    const { img, h2, location, catchPhrase, priceTag } = createMainElement();
+    return { img, h2, location, catchPhrase, priceTag };
+  };
+  return { name, picture, getUserCardDOM, getAuthorBlock };
+}
+
+const imageTemplate = (img) => {
+  // Fait le template des img de la page photographer
 }
