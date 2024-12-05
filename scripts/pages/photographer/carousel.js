96fa334b136a260a0867ebc4508736e25e000803
Timothe Lebrun <timothe463@icloud.com>
2024-12-05 19:27:15 +0100

    refactor-carousel: optimise carousel logic

--- scripts/pages/photographer/carousel.js
+++ scripts/pages/photographer/carousel.js
@@ -1,42 +1,44 @@
+/**
+ * @summary Handle the carousel prev/next logic.
+ * @param isNxt
+ */
 export const carouselArrow = (isNxt = false) => {
   const allCarItem = document.querySelectorAll(".modal-wp-view");
   const oldActive = document.querySelector("[data-active='true']");
-  const idActive = Number(oldActive.getAttribute("data-id"));
-  const nbItems = allCarItem.length - 1;
 
-  oldActive && oldActive.removeAttribute("data-active");
-  if (idActive == nbItems && isNxt) {
-    // Sup à 10
-    const activeArticle = document.querySelector("[data-id='0']");
-    activeArticle.setAttribute("data-active", true);
+  if (!oldActive)
+    return;
+  oldActive.removeAttribute("data-active");
+
+  const nbItems = allCarItem.length - 1;
+  const idActive = Number(oldActive.getAttribute("data-id"));
+  const direction = isNxt ? 1 : -1;
+  let newActiveId= 0;
+  if (idActive === nbItems && isNxt) {
+    // Get to the start of the carousel
     allCarItem.forEach((item) => {
       item.style.transform = "translateX(0)";
       item.setAttribute("data-offset", "0");
     });
-  } else if (idActive == 0 && !isNxt) {
-    // Inf à 0
-    const activeArticle = document.querySelector(
-      `[data-id='${Number(nbItems)}']`
-    );
-    activeArticle.setAttribute("data-active", true);
+  } else if (idActive === 0 && !isNxt) {
+    // Get to the end of the carousel
+    newActiveId= nbItems;
     allCarItem.forEach((item) => {
       item.style.transform = `translateX(calc(${nbItems * -1050}px))`;
       item.setAttribute("data-offset", `${nbItems * -1050}`);
     });
   } else {
-    // Etat normal
-    const activeArticle = isNxt
-      ? document.querySelector(`[data-id='${idActive + 1}']`)
-      : document.querySelector(`[data-id='${idActive - 1}']`);
-    oldActive && oldActive.removeAttribute("data-active");
-    activeArticle.setAttribute("data-active", true);
+    // Normal state
+    newActiveId = idActive + direction;
     allCarItem.forEach((item) => {
-      let itemOffSet = Number(item.getAttribute("data-offset"));
-      let calc = isNxt ? itemOffSet - 1050 : itemOffSet + 1050;
+      const itemOffSet = Number(item.getAttribute("data-offset"));
+      const calc = isNxt ? itemOffSet - 1050 : itemOffSet + 1050;
       item.style.transform = `translateX(${calc}px)`;
       item.setAttribute("data-offset", calc);
     });
   }
+  const activeArticle = document.querySelector(`[data-id='${newActiveId}']`);
+  activeArticle.setAttribute("data-active", true);
   const isVideo = document.querySelector("[data-active='true'] video");
   isVideo && isVideo.focus();
 };
